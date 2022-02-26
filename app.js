cd3c171a6c35dc7d60308c176bf34b4299de3457
Ashir N Basheer <ashirnbasheer@gmail.com>
2022-02-27 00:23:38 +0530

    almost finished

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const columns = document.querySelectorAll(".col");
 var letter;
 var i =1;
+var modal = document.getElementById("modal")
 winningPattern =[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
     [1,4,7],
     [2,5,8],
-    [3.6,9],
-    [1,5,9],
-    [3,5,7],
+    [0,4,8],
+    [2,4,6],
 ]
-
 const classfinder = () =>{
-
-    columns.forEach(col=>{
+columns.forEach(col=>{
         col.addEventListener("click",()=>{
             letter = 'x'
             if(i%2==0){
@@ -23,16 +22,35 @@ const classfinder = () =>{
             if(col.classList.contains("x") || (col.classList.contains("o"))){
                 return
             }
-            
-
             col.classList.add(letter)
             i++
+            checker()
         })
-        
-    })
-
+        })
+    }
+const checker = () =>{
+    for(let j=0;j<winningPattern.length;j++){
+        if(columns[winningPattern[j][0]].classList.contains("x") && columns[winningPattern[j][1]].classList.contains("x") && columns[winningPattern[j][2]].classList.contains("x")){
+            console.log("x wins")
+            const h2 = document.createElement("h2");
+            const text = document.createTextNode("X wins");
+            h2.appendChild(text)
+            modal.appendChild(h2)
+            modal.style.display= "block"
+            
+        }else if(columns[winningPattern[j][0]].classList.contains("o") && columns[winningPattern[j][1]].classList.contains("o") && columns[winningPattern[j][2]].classList.contains("o")){
+            console.log("o wins")
+            const h2 = document.createElement("h2");
+            const text = document.createTextNode("O wins");
+            h2.appendChild(text)
+            modal.appendChild(h2)
+            modal.style.display= "block"
+        }
 }
- const xandoAdder =() =>{
-     
- }
-classfinder()
+
+gameover()
+}
+const gameover =()=>{
+ 
+}
+ classfinder()
